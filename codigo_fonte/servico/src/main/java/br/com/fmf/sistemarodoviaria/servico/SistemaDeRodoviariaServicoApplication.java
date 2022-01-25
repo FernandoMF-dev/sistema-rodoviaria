8b4bf32e8780886709e4c977920a63c21c498ec9
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-01-25 11:40:52 -0300

    Criando projeto

---
+++ codigo_fonte/servico/src/main/java/br/com/fmf/sistemarodoviaria/servico/SistemaDeRodoviariaServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmf.sistemarodoviaria.servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeRodoviariaServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeRodoviariaServicoApplication.class, args);
+	}
+
+}
---
+++ codigo_fonte/servico/src/test/java/br/com/fmf/sistemarodoviaria/servico/SistemaDeRodoviariaServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmf.sistemarodoviaria.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeRodoviariaServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
