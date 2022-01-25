41b829a78b095d0cf6a2bf1f46727ec6b6f1d0a8
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-01-25 15:26:47 -0300

    Criando segundo micro serviçoo

---
+++ codigo_fonte/documento/src/main/java/br/com/fmf/sistemarodoviaria/documento/SistemaDeRodoviariaDocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmf.sistemarodoviaria.documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeRodoviariaDocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeRodoviariaDocumentoApplication.class, args);
+	}
+
+}
