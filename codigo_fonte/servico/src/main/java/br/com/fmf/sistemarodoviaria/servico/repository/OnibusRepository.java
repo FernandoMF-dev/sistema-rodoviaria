a6479e5465e6164b5fd8395bbf79a065f93a4ce5
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-01-27 12:22:01 -0300

    Implementando OnibusService e resolvendo problema na dependencia do mapstruct
    Signed-off-by: Fernando Martins Ferreira <fernando.ferreira@basis.com.br>

--- codigo_fonte/servico/src/main/java/br/com/fmf/sistemarodoviaria/servico/repository/OnibusRepository.java
+++ codigo_fonte/servico/src/main/java/br/com/fmf/sistemarodoviaria/servico/repository/OnibusRepository.java
@@ -3,6 +3,19 @@ package br.com.fmf.sistemarodoviaria.servico.repository;
 import br.com.fmf.sistemarodoviaria.servico.domain.entity.Onibus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OnibusRepository extends JpaRepository<Onibus, Long>, JpaSpecificationExecutor<Onibus> {
+	List<Onibus> findOnibusByExcluidoIsFalse();
+
+	Optional<Onibus> findByIdAndExcluidoIsFalse(Long id);
+
+	@Modifying
+	@Query("UPDATE Onibus o SET o.excluido = TRUE WHERE o.id = :id")
+	void delete(@Param("id") Long id);
 }
---
+++ codigo_fonte/servico/src/main/java/br/com/fmf/sistemarodoviaria/servico/service/OnibusService.java
@@ -0,0 +1,55 @@
+package br.com.fmf.sistemarodoviaria.servico.service;
+
+import br.com.fmf.sistemarodoviaria.servico.domain.entity.Onibus;
+import br.com.fmf.sistemarodoviaria.servico.repository.OnibusRepository;
+import br.com.fmf.sistemarodoviaria.servico.service.dto.OnibusDto;
+import br.com.fmf.sistemarodoviaria.servico.service.exception.RegraNegocioException;
+import br.com.fmf.sistemarodoviaria.servico.service.mapper.OnibusMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+@Transactional
+public class OnibusService {
+
+	private final OnibusMapper onibusMapper;
+	private final OnibusRepository onibusRepository;
+
+	public OnibusDto insert(OnibusDto dto) {
+		Onibus entity = onibusMapper.toEntity(dto);
+
+		entity.setId(null);
+		entity.setExcluido(false);
+
+		return save(entity);
+	}
+
+	public OnibusDto update(OnibusDto dto) {
+		Onibus entity = onibusMapper.toEntity(dto);
+		return save(entity);
+	}
+
+	public List<OnibusDto> findAll() {
+		return onibusMapper.toDto(onibusRepository.findOnibusByExcluidoIsFalse());
+	}
+
+	public OnibusDto findById(Long id) {
+		return onibusMapper.toDto(onibusRepository.findByIdAndExcluidoIsFalse(id)
+				.orElseThrow(() -> new RegraNegocioException("Registro não encontrado")));
+	}
+
+	public void delete(Long id) {
+		onibusRepository.delete(id);
+	}
+
+	private OnibusDto save(Onibus entity) {
+		return onibusMapper.toDto(onibusRepository.save(entity));
+	}
+
+}
