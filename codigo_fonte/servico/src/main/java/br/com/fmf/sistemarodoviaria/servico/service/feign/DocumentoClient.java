96b866c404ee10e56f9da5f06fdd42366726d726
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-04-08 09:03:27 -0300

    Implementando DocumentoClient
    Signed-off-by: Fernando Martins Ferreira <fernando.ferreira@basis.com.br>

--- codigo_fonte/servico/src/main/java/br/com/fmf/sistemarodoviaria/servico/service/feign/DocumentoClient.java
+++ codigo_fonte/servico/src/main/java/br/com/fmf/sistemarodoviaria/servico/service/feign/DocumentoClient.java
@@ -1,7 +1,26 @@
 package br.com.fmf.sistemarodoviaria.servico.service.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "documento", path = "/api/documento", url = "http://localhost:8081")
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+@FeignClient(name = "documento", path = "/api/documento", url = "${application.module.documento}")
 public interface DocumentoClient {
+
+	@PostMapping(consumes = {MULTIPART_FORM_DATA_VALUE})
+	UUID create(MultipartFile file);
+
+	@DeleteMapping(value = "{uuid}")
+	void delete(@PathVariable UUID uuid);
+
+	@GetMapping(value = "{uuid}")
+	byte[] find(@PathVariable UUID uuid);
+
 }
