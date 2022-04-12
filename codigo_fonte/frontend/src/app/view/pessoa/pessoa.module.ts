45eb078d31c750aff1015151ee2e2cbca5b1d5d3
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-04-12 10:19:38 -0300

    Criando PessoaComponent
    Signed-off-by: Fernando Martins Ferreira <fernando.ferreira@basis.com.br>

--- codigo_fonte/frontend/src/app/view/pessoa/pessoa.module.ts
+++ codigo_fonte/frontend/src/app/view/pessoa/pessoa.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { PessoaRoutingModule } from './pessoa-routing.module';
+import { PessoaComponent } from './pessoa.component';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PessoaRoutingModule
-  ]
+	declarations: [PessoaComponent],
+	imports: [
+		CommonModule,
+		PessoaRoutingModule
+	]
 })
-export class PessoaModule { }
+export class PessoaModule {
+}
