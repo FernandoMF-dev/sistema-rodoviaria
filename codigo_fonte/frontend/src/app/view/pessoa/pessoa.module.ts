bb6c9106928c46b7c9d50424a832da8b87a91c46
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-04-12 15:35:54 -0300

    Criando PessoaCadastroComponent
    Signed-off-by: Fernando Martins Ferreira <fernando.ferreira@basis.com.br>

--- codigo_fonte/frontend/src/app/view/pessoa/pessoa.module.ts
+++ codigo_fonte/frontend/src/app/view/pessoa/pessoa.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { PessoaListComponent } from './pessoa-list/pessoa-list.component';
 
 import { PessoaRoutingModule } from './pessoa-routing.module';
@@ -8,7 +9,7 @@ import { PessoaComponent } from './pessoa.component';
 
 
 @NgModule({
-	declarations: [PessoaComponent, PessoaListComponent],
+	declarations: [PessoaComponent, PessoaListComponent, PessoaCadastroComponent],
 	imports: [
 		CommonModule,
 		PessoaRoutingModule,
