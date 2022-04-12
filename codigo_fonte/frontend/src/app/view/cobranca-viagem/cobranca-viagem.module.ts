de64718aa76e4875a6480de2f3525c83445e93c4
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-04-12 10:42:40 -0300

    Criando OnibusRoutingModule, PasseRoutingModule e CobrancaViagemRoutingModule
    Signed-off-by: Fernando Martins Ferreira <fernando.ferreira@basis.com.br>

---
+++ codigo_fonte/frontend/src/app/view/cobranca-viagem/cobranca-viagem.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CobrancaViagemRoutingModule } from './cobranca-viagem-routing.module';
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		CobrancaViagemRoutingModule
+	]
+})
+export class CobrancaViagemModule {
+}
---
+++ codigo_fonte/frontend/src/app/view/passe/passe.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PasseRoutingModule } from './passe-routing.module';
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		PasseRoutingModule
+	]
+})
+export class PasseModule {
+}
